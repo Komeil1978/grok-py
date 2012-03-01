85432779fb2eea3049053696c36a90f5bf206d77
Ian Danforth <idanforth@numenta.com>
2012-02-29 16:27:31 -0800

    Add initial Sphinx docs test

--- grokpy/__init__.py
+++ grokpy/__init__.py
@@ -1,15 +1,21 @@
-import httplib2
 from client import (Client,
                    Aggregation,
                    Status,
                    GrokData,
                    DataFlag)
+from connection import Connection
+from field import Field
+from model import Model
+from project import Project
+from publicDataSource import PublicDataSource
+from stream import Stream
+from streaming import StreamListener, StreamMonitor
 
 from exceptions import (AuthenticationError,
                         GrokError)
 
-from streaming import StreamMonitor
-
-
 __version__ = '0.1.0'
+
+__all__ = ['Client', 'Connection','Field','Model','Project',
+           'PublicDataSource','Stream','StreamListener', 'StreamMonitor']
 
